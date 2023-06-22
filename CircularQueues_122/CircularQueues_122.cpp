3e9d228e5eb98c9ca69288f71be11c705555cc04
mrendy58 <derendyyy58@gmail.com>
2023-06-22 15:54:55 +0700

    membuat cout option

--- CircularQueues_122/CircularQueues_122.cpp
+++ CircularQueues_122/CircularQueues_122.cpp
@@ -100,8 +100,47 @@ public:
     }
 };
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main(){
+    Queues q;
+    char ch;
+
+    while (true) {
+        try {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement delete operation" << endl;
+            cout << "3. Display Values" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choice (1-4)" << endl;
+            cin >> ch;
+            cout << endl;
+
+            switch (ch) {
+            case '1': {
+                q.insert();
+                break;
+            }
+            case '2': {
+                q.remove();
+                break;
+            }
+            case '3': {
+                q.display();
+                break;
+            }
+            case '4': {
+                return;
+            }
+            default: {
+                cout << "Invalid option!!" << endl;
+                break;
+            }
+            }
+        }
+        catch (exception& e) {
+            cout << "check for the value entered." << endl;
+        }
+    }
+    return;
 }
 
