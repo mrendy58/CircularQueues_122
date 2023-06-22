39357257ce6a7a6e388bbcd670d261692f0431ae
mrendy58 <derendyyy58@gmail.com>
2023-06-22 15:20:37 +0700

    membuat fungsi untuk mengecek antrian hanya memiliki satu element

--- CircularQueues_122/CircularQueues_122.cpp
+++ CircularQueues_122/CircularQueues_122.cpp
@@ -46,7 +46,11 @@ public:
         }
         cout << "\nThe element deleted from the queue is :" << queue_array[FRONT] << "\n";
 
-
+        // cek jika antrian hanya memiliki satu element
+        if (FRONT == REAR) {
+            FRONT = -1;
+            REAR = -1;
+        }
     }
 };
 
