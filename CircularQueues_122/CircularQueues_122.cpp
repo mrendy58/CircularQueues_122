af27df900c75c929363ca8668c4f18a6eb8e2d78
mrendy58 <derendyyy58@gmail.com>
2023-06-22 15:23:13 +0700

    membuat fungsi jika front berada di akhir array

--- CircularQueues_122/CircularQueues_122.cpp
+++ CircularQueues_122/CircularQueues_122.cpp
@@ -51,6 +51,13 @@ public:
             FRONT = -1;
             REAR = -1;
         }
+        else {
+            // jika element yang dihapus berada di posisi terakhir array, kembali ke awal array
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
+        }
     }
 };
 
